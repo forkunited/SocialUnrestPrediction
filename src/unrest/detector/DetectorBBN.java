75a3b3da28cb1fa6b7010b5986fc2c16db41976f
Bill McDowell <forkunited@gmail.com>
2013-10-21 13:35:09 -0400

    removed detectors for test

--- src/unrest/detector/DetectorBBN.java
+++ src/unrest/detector/DetectorBBN.java
@@ -11,10 +11,10 @@ public class DetectorBBN extends DetectorConjunction {
 		UnrestProperties properties = new UnrestProperties();
 		
 		Gazetteer unrestTermGazetteer = new Gazetteer("UnrestTerm", properties.getUnrestTermGazetteerPath(), StringUtil.getDefaultCleanFn());
-		Gazetteer unrestLocationGazetteer = new Gazetteer("UnrestLocation", properties.getUnrestLocationGazetteerPath(), StringUtil.getDefaultCleanFn());
+		//Gazetteer unrestLocationGazetteer = new Gazetteer("UnrestLocation", properties.getUnrestLocationGazetteerPath(), StringUtil.getDefaultCleanFn());
 		
 		this.detectors.add(new DetectorGazetteer(unrestTermGazetteer, false));
-		this.detectors.add(new DetectorGazetteer(unrestLocationGazetteer, true));
-		this.detectors.add(new DetectorFutureDate());
+		//this.detectors.add(new DetectorGazetteer(unrestLocationGazetteer, true));
+		//this.detectors.add(new DetectorFutureDate());
 	}
 }
